4ebd0d2c9ff508d1bc98b8211f4869f9e9023ac3
Jacob Hughes <jh@jakehughes.uk>
2020-03-24 18:29:53 +0000

    Only rustfmt on CI if it builds on latest nightly

--- .buildbot.sh
+++ .buildbot.sh
@@ -10,8 +10,15 @@ sh rustup.sh --default-host x86_64-unknown-linux-gnu --default-toolchain nightly
 
 export PATH=`pwd`/.cargo/bin/:$PATH
 
-rustup component add --toolchain nightly rustfmt-preview || cargo +nightly install --force rustfmt-nightly
-
-cargo +nightly fmt --all
+# Sometimes rustfmt is so broken that there is no way to install it at all.
+# Rather than refusing to merge, we just can't rust rustfmt at such a point.
+rustup component add --toolchain nightly rustfmt-preview \
+    || cargo +nightly install --force rustfmt-nightly \
+    || true
+rustfmt=0
+cargo fmt 2>&1 | grep "not installed for the toolchain" > /dev/null || rustfmt=1
+if [ $rustfmt -eq 1 ]; then
+    cargo +nightly fmt --all -- --check
+fi
 
 cargo check
