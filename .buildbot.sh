950b74608e83b34a30a8bc24aa9e4e8337a89f62
Laurence Tratt <laurie@tratt.net>
2020-07-16 16:52:41 +0100

    Use the "get the last working version of rustfmt" trick.

--- .buildbot.sh
+++ .buildbot.sh
@@ -6,19 +6,11 @@ export CARGO_HOME="`pwd`/.cargo"
 export RUSTUP_HOME="`pwd`/.rustup"
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rustup.sh
-sh rustup.sh --default-host x86_64-unknown-linux-gnu --default-toolchain nightly -y --no-modify-path
+# Install no toolchain initially to ensure toolchain rollback.
+sh rustup.sh --default-host x86_64-unknown-linux-gnu --default-toolchain none -y --no-modify-path
 
 export PATH=`pwd`/.cargo/bin/:$PATH
+rustup install nightly
 
-# Sometimes rustfmt is so broken that there is no way to install it at all.
-# Rather than refusing to merge, we just can't rust rustfmt at such a point.
-rustup component add --toolchain nightly rustfmt-preview \
-    || cargo +nightly install --force rustfmt-nightly \
-    || true
-rustfmt=0
-cargo fmt 2>&1 | grep "not installed for the toolchain" > /dev/null || rustfmt=1
-if [ $rustfmt -eq 1 ]; then
-    cargo +nightly fmt --all -- --check
-fi
-
+cargo +nightly fmt --all -- --check
 cargo check
