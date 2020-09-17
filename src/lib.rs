0fbad41bf8696accd5acf836c3caf3576858bed1
Jacob Hughes <jh@jakehughes.uk>
2020-09-17 13:45:00 +0100

    Fix Gc<T> to only finalize values when T needs drop
    There were two problems with our previous approach. First, relying
    exclusively on a `NoFinalize` trait to prevent finalization is
    error-prone and only applies to a small subset of types where
    finalization can be optimized with the `mem::needs_drop` intrinsic.
    
    Second, a casting bug in `assume_init` meant that `register_finalizer`
    was being passed a `MaybeUninit<T>` instead of `T`. The former will
    always fail the `needs_drop` check, so finalizers were never being
    registered. This can cause memory leaks. This issue was prevelant in a
    common pattern where `layout_from_new` is used with Narrow Trait
    Objects.

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,6 @@
 #![feature(coerce_unsized)]
 #![feature(unsize)]
 #![feature(maybe_uninit_ref)]
-#![feature(specialization)]
 #[cfg(not(all(target_pointer_width = "64", target_arch = "x86_64")))]
 compile_error!("Requires x86_64 with 64 bit pointer width.");
 
